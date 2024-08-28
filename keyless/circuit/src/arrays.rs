c88179def25f4645849561cad7b9168d36674eaa
Michael Straka <mstraka100@gmail.com>
2024-08-28 17:34:58 -0500

    unit tests improved

--- keyless/circuit/src/arrays.rs
+++ keyless/circuit/src/arrays.rs
@@ -9,6 +9,7 @@ use aptos_keyless_common::input_processing::{
 use ark_bn254::Fr;
 use ark_ff::{One, Zero};
 use rand::{thread_rng, Rng};
+use std::num;
 
 fn build_array_selector_output(len: u32, start: u32, end: u32) -> Vec<u8> {
     let len = len as usize;
@@ -24,44 +25,60 @@ fn build_invert_binary_array_output(input: Vec<bool>) -> Vec<bool> {
 #[test]
 fn invert_binary_array_test() {
     let circuit_handle = TestCircuitHandle::new("arrays/invert_binary_array_test.circom").unwrap();
-    let test_case = [false, false, true, true];
-    let output = build_invert_binary_array_output(test_case.to_vec());
-    let config =
-        CircuitPaddingConfig::new().max_length("in", output.len() as usize).max_length("expected_out", output.len() as usize);
-    let circuit_input_signals = CircuitInputSignals::new()
-        .bytes_input("in", &test_case.into_iter().map(|x| x as u8).collect::<Vec<u8>>()[..])
-        .bytes_input("expected_out", &output.into_iter().map(|x| x as u8).collect::<Vec<u8>>()[..])
-        .pad(&config)
-        .unwrap();
-    let result = circuit_handle.gen_witness(circuit_input_signals);
-    println!("{:?}", result);
-    assert!(result.is_ok());
+
+    for b0 in [true, false] {
+       for b1 in [true, false] {
+           for b2 in [true,false] {
+               for b3 in [true, false] {
+                   let test_case = [b0, b1, b2, b3];
+                   let output = build_invert_binary_array_output(test_case.to_vec());
+                   let config =
+                       CircuitPaddingConfig::new().max_length("in", output.len() as usize).max_length("expected_out", output.len() as usize);
+                   let circuit_input_signals = CircuitInputSignals::new()
+                       .bytes_input("in", &test_case.into_iter().map(|x| x as u8).collect::<Vec<u8>>()[..])
+                       .bytes_input("expected_out", &output.into_iter().map(|x| x as u8).collect::<Vec<u8>>()[..])
+                       .pad(&config)
+                       .unwrap();
+                   let result = circuit_handle.gen_witness(circuit_input_signals);
+                   println!("{:?}", result);
+                   assert!(result.is_ok());
+               }
+           }
+       }
+    }
+
 }
 
 #[test]
-// TODO: Make unit test more comprehensive
 fn elementwise_mul_test() {
     let circuit_handle = TestCircuitHandle::new("arrays/elementwise_mul_test.circom").unwrap();
-    let test_case_left = [5, 2, 3, 4];
-    let test_case_right = [3, 2, 3, 4];
-    let output: Vec<u8> = test_case_left.into_iter().zip(test_case_right).map(|(x,y)| x * y).collect();
-    let config =
-        CircuitPaddingConfig::new().max_length("left", output.len() as usize)
-                                   .max_length("right", output.len() as usize)
 
-                                   .max_length("expected_out", output.len() as usize);
-    let circuit_input_signals = CircuitInputSignals::new()
-        .bytes_input("left", &test_case_left)
-        .bytes_input("right", &test_case_right)
-        .bytes_input("expected_out", &output)
-        .pad(&config)
-        .unwrap();
-    let result = circuit_handle.gen_witness(circuit_input_signals);
-    assert!(result.is_ok());
+    let mut rng = rand::thread_rng();
+    for _i in 0..25 {
+            let test_case_left: Vec<u64> = (0..4).map(|_| rng.gen_range(0, (u64::MAX as f64).sqrt() as u64)).collect();
+            let test_case_right: Vec<u64> = (0..4).map(|_| rng.gen_range(0, (u64::MAX as f64).sqrt() as u64)).collect();
+            let output: Vec<u64> = test_case_left.clone().into_iter().zip(test_case_right.clone()).map(|(x,y)| x * y).collect();
+            let test_case_left_frs: Vec<Fr> = test_case_left.into_iter().map(|i| Fr::from(i)).collect();
+            let test_case_right_frs: Vec<Fr> = test_case_right.into_iter().map(|i| Fr::from(i)).collect();
+            let output_frs: Vec<Fr> = output.clone().into_iter().map(|i| Fr::from(i)).collect();
+
+            let config =
+                CircuitPaddingConfig::new().max_length("left", output.len() as usize)
+                                        .max_length("right", output.len() as usize)
+
+                                        .max_length("expected_out", output.len() as usize);
+            let circuit_input_signals = CircuitInputSignals::new()
+                .frs_input("left", &test_case_left_frs)
+                .frs_input("right", &test_case_right_frs)
+                .frs_input("expected_out", &output_frs)
+                .pad(&config)
+                .unwrap();
+            let result = circuit_handle.gen_witness(circuit_input_signals);
+            assert!(result.is_ok());
+    }
 }
 
 #[test]
-// TODO: Make unit test more comprehensive
 fn array_selector_test() {
     let circuit_handle = TestCircuitHandle::new("arrays/array_selector_test.circom").unwrap();
     let out_len = 8;
