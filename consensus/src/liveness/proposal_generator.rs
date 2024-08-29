027384c5092bba21908a1449c1f6bc4e931eb6ab
danielxiangzl <xiangzhuolun@gmail.com>
2024-08-29 12:06:14 -0700

    revert qs timeout

--- consensus/src/liveness/proposal_generator.rs
+++ consensus/src/liveness/proposal_generator.rs
@@ -510,18 +510,18 @@ impl ProposalGenerator {
         );
 
         // Check if the block contains any randomness transaction
-        // let maybe_require_randomness = skip_non_rand_blocks.then(|| {
-        //     all_txns.par_iter().any(|txns| {
-        //         self.validator.read().check_randomness_in_batch(txns)
-        //     })
-        // });
-
         let maybe_require_randomness = skip_non_rand_blocks.then(|| {
-            all_txns
-                .par_iter()
-                .flatten()
-                .any(|txns| txns.par_iter().any(|txn| self.validator.read().check_randomness(txn)))
+            all_txns.par_iter().any(|txns| {
+                self.validator.read().check_randomness_in_batch(txns)
+            })
         });
+
+        // let maybe_require_randomness = skip_non_rand_blocks.then(|| {
+        //     all_txns
+        //         .par_iter()
+        //         .flatten()
+        //         .any(|txns| txns.par_iter().any(|txn| self.validator.read().check_randomness(txn)))
+        // });
 
 
         observe_block(timestamp, BlockStage::CHECKED_RAND);
